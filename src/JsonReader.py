9f7e85f5f71470231a45d9760345c3cedaf6f158
Ali Maamoun <alimaamoun@gmail.com>
2024-03-06 23:13:25 -0800

    just quickly added a simple template of how we can write to json files. Should implement further each class structure's json file type and how each will look depending on their attributes to be saved. TODO: need to read the json file and implement it into an object, example, read json file into an account object.

---
+++ src/JsonReader.py
@@ -0,0 +1,22 @@
+import json
+
+class jsonwriter:
+
+    def __init__(self,obj):
+        self._data : dict = obj
+
+
+    def write(self):
+        jsonObj = json.dumps(self._data, indent=4)
+
+        #writing to sample .json
+        with open("data/sample.json", "w") as outfile:
+            outfile.write(jsonObj)
+
+
+    def read(self):
+        pass
+
+
+example : dict = {"Properties":{"property1":{"Tenant":{"name" : "Ridham Patel"}},"property2":{"Tenant":{}},"property3":{"Tenant":{}}}}
+jsonwriter(example).write()
