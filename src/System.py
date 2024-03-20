c836c839a7def6848bd5b8631b5305a68223c17c
Ali Maamoun <alimaamoun@gmail.com>
2024-03-19 17:28:41 -0700

    System Class changes

--- src/System.py
+++ src/System.py
@@ -1,7 +1,19 @@
 import Entity, Account, Property, Tenant
 
 class System():
-    pass
+    def init():
+        pass
+
+    def StartSession():
+        #TODO:
+        #take in username and password and find corresponding Object
+        #load data from JSON file and create Account Object
+        pass
+
+    def EndSession():
+        #Save data from Account object into JSON file
+        pass
+
 
 
 def main() -> None:
