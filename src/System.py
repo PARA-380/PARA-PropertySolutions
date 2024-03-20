52f1c9a3e61448a5a9b91a348a94f3f583be5fb6
Ali Maamoun <alimaamoun@gmail.com>
2024-03-19 17:55:41 -0700

    Small changes to let myself branch

--- src/System.py
+++ src/System.py
@@ -1,8 +1,15 @@
-import Entity, Account, Property, Tenant
+from Entity import Entity
+from Account import Account
+from Property import Property
+from Tenant import Tenant
 
 class System():
-    def init():
+    def __init__(self):
+        self.Accounts : Account = {}
         pass
+
+    def __repr__(self):
+        return f"Accounts: {self.Accounts}"
 
     def StartSession():
         #TODO:
@@ -17,7 +24,10 @@ class System():
 
 
 def main() -> None:
+    session1 = System()
+    print(session1.Accounts)
     pass
 
 if __name__ == "__main__":
+    main()
     pass
