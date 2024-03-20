2727670c6d30ff255236e76171c92b5beff65d45
Adrian <adrian.carrreno.807@my.csun.edu>
2024-03-19 22:07:38 -0700

    added getter and setter methods to account class

--- src/Account.py
+++ src/Account.py
@@ -1,24 +1,54 @@
 # from JsonReader import jsonwriter as json
-import array
+# import array
 from Entity import Entity
 from Tenant import Tenant
 
 
 class Account(Entity):
     def __init__(self, name: str = "", username: str = "", password: str = "", tenants: list = None, total_revenue={},
-                 dashboard={}):
+                 dashboard={}, contractors: list = None):
         self.name = name
         self.username = username
         self.password = password  # might want to implement a security feature for storing passwords in json files
         self.properties = {}
         self.tenants = [] if tenants is None else tenants
-        self.contractors = {}
+        self.contractors = [] if contractors is None else contractors
 
     def addTenant(self, var):
         self.tenants.append(var)
 
-    def addContractor(self):
-        pass
+    def addContractor(self, var):
+        self.contractors.append(var)
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def set_username(self, username):
+        self.username = username
+
+    def get_properties(self):
+        for p in self.properties:
+            print(p)
+
+    def get_tenants(self):
+        for t in self.tenants:
+            print(t)
+
+    def get_username(self):
+        return self.username
+
+    def set_password(self, password):
+        self.password = password
+
+    def get_password(self):
+        return self.password
+
+    def get_contractors(self):
+        for c in self.contractors:
+            print(c)
 
     def to_dict(self):
         return {
@@ -38,10 +68,6 @@ def main() -> None:
     person = Tenant("Ridham")
     person2 = Tenant("Adrian", "Carreno", "012-34-5678", "AHHHHHH", "(805) xxx - xxxx")
     user.addTenant(person)
-    print(user.toJSON())
-    user.addTenant(person2)
-    print()
-    print(user.toJSON())
 
 
 if __name__ == "__main__":
