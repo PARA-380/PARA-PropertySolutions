41ca0d1eb19944355c0086cc90c21d9ba26a8492
Patrick2544 <jitta.patrick@gmail.com>
2024-04-05 11:19:59 -0700

    links the dashboard page with setting page

--- Dashboard_Page.py
+++ Dashboard_Page.py
@@ -13,6 +13,7 @@ from PyQt6.QtWidgets import (
 from gui.pyqt_dashboard import Ui_DashBoard
 from gui.UserProfile import Userprofile
 from Property_GUI.Property_main import Property_Page
+from Setting_GUI.Setting_Page_GUI import Setting_Page
 
 
 class Dashboard(QMainWindow, Ui_DashBoard):
@@ -23,6 +24,8 @@ class Dashboard(QMainWindow, Ui_DashBoard):
         self.Properties_btn.clicked.connect(self.goto_property_page)
         # pushButton = userprofile page
         self.pushButton_3.clicked.connect(self.goto_user_profile_page)
+        # pushButton_2 = setting page
+        self.pushButton_2.clicked.connect(self.goto_setting_page)
 
     def goto_property_page(self, checked):
         self.property_page = Property_Page()
@@ -31,3 +34,7 @@ class Dashboard(QMainWindow, Ui_DashBoard):
     def goto_user_profile_page(self, checked):
         self.user_profile = Userprofile()
         self.user_profile.show()
+
+    def goto_setting_page(self, checked):
+        self.setting_page = Setting_Page()
+        self.setting_page.show()
