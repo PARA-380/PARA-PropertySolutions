abeabed58dfae9c171c1c5e8ff5974a24ad1db90
Patrick2544 <jitta.patrick@gmail.com>
2024-04-04 19:03:24 -0700

    updates gui (calling files)

--- gui/UserProfile.py
+++ gui/UserProfile.py
@@ -4,7 +4,7 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtCore import Qt
 import sys
 #HelloWorld
-from User_Profile import Ui_MainWindow
+from gui.User_Profile import Ui_MainWindow
 
 class Userprofile(QMainWindow, Ui_MainWindow):
     def __init__(self):
