33424372a0ee0fedc3d877aef8b6e6654c28803c
Patrick2544 <jitta.patrick@gmail.com>
2024-04-12 15:06:05 -0700

    link login to dashboard

--- src/Login_Page.py
+++ src/Login_Page.py
@@ -5,8 +5,9 @@ from PyQt6.QtWidgets import (
 
 import sys
 
-from Login_Page_GUI.pyqt_Login_Page import Ui_Form
-from Login_Page_GUI.Signup_Page import SignUp_Page
+from src.gui.Login_Page_GUI.pyqt_Login_Page import Ui_Form
+from src.gui.Login_Page_GUI.Signup_Page import SignUp_Page
+from src.Dashboard_Page import Dashboard
 
 class Login_Page(QMainWindow, Ui_Form):
     def __init__(self):
@@ -17,18 +18,23 @@ class Login_Page(QMainWindow, Ui_Form):
         # login_btn_2 = sign up button
         self.login_btn_2.clicked.connect(self.go_to_signup_page)
 
-        # login_btn = sign in button
-        self.login_btn.clicked.connect(self.sing_in)
+        # login_btn = log in button
+        self.login_btn.clicked.connect(self.log_in)
 
     def go_to_signup_page(self):
         self.signup_page = SignUp_Page()
         self.signup_page.show()
 
-    def sing_in(self):
+    def log_in(self):
         # Add is sign in success def here to check
-        self.close()
+        print("login in")
         print(self.username_line_edit.text())
         print(self.password_line_edit.text())
+        if self.username_line_edit.text() == "1" and self.password_line_edit.text() == "2":
+            print("log in success")
+            self.hide()
+            dashboard = Dashboard()
+            dashboard.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
