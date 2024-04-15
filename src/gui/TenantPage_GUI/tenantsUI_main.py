7efb6d9d0fe2bf845e1feffdb5fecd0f0522ab38
Ridham Patel <Ridhamsangani.23@gmail.com>
2024-04-15 16:09:48 -0700

    Tenants page GUI updated to allow for adding Tenants to account. Need to add to Database

--- src/gui/TenantPage_GUI/tenantsUI_main.py
+++ src/gui/TenantPage_GUI/tenantsUI_main.py
@@ -3,8 +3,6 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtCore import Qt
 import sys
-# from tenantsUI_Controller import TenantsUI
-# from src.gui.TenantPage_GUI. import Tenant
 
 from src.gui.TenantPage_GUI.QtTenantPage import Ui_MainWindow
 
@@ -13,10 +11,38 @@ class tenantsui(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.show()
+        self.AddTenant.clicked.connect(self.Add_Tenant)
+        self.DeleteTenant.clicked.connect(self.Delete_Tenant)  # Connect the Delete Tenant button
         self.tableWidget.setColumnWidth(4, 600)
         self.tableWidget.setColumnWidth(2, 300)
         self.tableWidget.setColumnWidth(1, 200)
         self.tableWidget.setColumnWidth(3, 200)
+
+    def Add_Tenant(self):
+        First_Name =  self.LE_FirstName.text()
+        Last_Name = self.LE_LastName.text()
+        SSN = self.LE_SSN.text()
+        Phone_Number = self.LE_PhoneNumber.text()
+        Email = self.LE_Email.text()
+
+        # Insert a new row at the end of the tableWidget
+        row_position = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(row_position)
+
+        # Add the data to the new row
+        self.tableWidget.setItem(row_position, 0, QTableWidgetItem(First_Name))
+        self.tableWidget.setItem(row_position, 1, QTableWidgetItem(Last_Name))
+        self.tableWidget.setItem(row_position, 2, QTableWidgetItem(SSN))
+        self.tableWidget.setItem(row_position, 3, QTableWidgetItem(Phone_Number))
+        self.tableWidget.setItem(row_position, 4, QTableWidgetItem(Email))
+
+
+    def Delete_Tenant(self):
+        selected_row = self.tableWidget.currentRow()  # Get the index of the currently selected row
+        if selected_row >= 0:  # Check if a row is selected (index is valid)
+            self.tableWidget.removeRow(selected_row)  # Remove the selected row
+        else:
+            QMessageBox.warning(self, "Warning", "Please select a row to delete", QMessageBox.StandardButton.Ok)
 
 
 if __name__ == "__main__":
