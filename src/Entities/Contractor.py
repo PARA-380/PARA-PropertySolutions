3351ebad51a59c9ab8e0e53abb33b0c5c4d94f84
Patrick2544 <jitta.patrick@gmail.com>
2024-05-17 16:20:12 -0700

    add minor things

--- src/Entities/Contractor.py
+++ src/Entities/Contractor.py
@@ -1,4 +1,10 @@
-
+"""
+File: Contractor.py
+Name: Jittapatana Prayoonpruk
+Date: 04/27/24
+Description: Handles Contractor attributes
+Purposes: Creates a basis for a Contractor and its attributes.  Attributes get handled by its controller.
+"""
 
 class Contractor:
     def __init__(self, accID : int = None, ID : int = None, specialization: str = "", firstname: str = "", lastname: str = "", phonenumber: str = "",):
@@ -15,19 +21,49 @@ class Contractor:
         phone-number: {self.contractor_phone} \n"""
 
     def get_first_name(self):
+        """get contractor's first name
+
+        Returns:
+            string: contractor's first name
+        """
         return self.contractor_firstname
     
     def get_last_name(self):
+        """get contractor's last name
+
+        Returns:
+            string: contractor's last name
+        """
         return self.contractor_lastname
     
     def get_specialization(self):
+        """get contractor's specialization
+
+        Returns:
+            string: contractor's specialization
+        """
         return self.contractor_specialization
     
     def get_phone_number(self):
+        """get contractor's phone number
+
+        Returns:
+            string: contractor's phone number
+        """
         return self.contractor_phone
     
     def get_contractor_id(self):
+        """get contractor's ID
+
+        Returns:
+            int: contractor's ID
+        """
         return self.contractor_id
     
     def set_id(self, id):
+        """set contractor's ID
+
+        Args:
+            id (int): contractor's ID
+        """
         self.contractor_id = id
